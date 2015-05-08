d772adab43d1cd3506bce6038c60d7c5bfb4075a
sistemasroble03 <sistemasroble03@gmail.com>
2015-05-08 13:35:04 -0500

    'blbl'

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic']).run(function($ionicPlatform) {
+angular.module('starter', ['ionic']).run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -14,90 +14,25 @@ var app = angular.module('starter', ['ionic']).run(function($ionicPlatform) {
       StatusBar.styleDefault();
     }
   });
-});
+})
 
-
-/**
- * transform request factory.
- */
-app.factory("transformRequestAsFormPost", function() {
-
-		// I prepare the request data for the form post.
-		function transformRequest( data, getHeaders ) {
-
-			var headers = getHeaders();
-
-			// headers[ "Content-Type" ] = "application/x-www-form-urlencoded";
-
-			return( serializeData( data ) );
-
-		}
-
-
-		// Return the factory value.
-		return( transformRequest );
-
-		function serializeData( data ) {
-
-			// If this is not an object, defer to native stringification.
-			if ( ! angular.isObject( data ) ) {
-
-				return( ( data == null ) ? "" : data.toString() );
-
-			}
-
-			var buffer = [];
-
-			// Serialize each key in the object.
-			for ( var name in data ) {
-
-				if ( ! data.hasOwnProperty( name ) ) {
-
-					continue;
-
-				}
-
-				var value = data[ name ];
-
-				buffer.push(
-					encodeURIComponent( name ) +
-					"=" +
-					encodeURIComponent( ( value == null ) ? "" : value )
-				);
-
-			}
-
-			// Serialize the buffer and clean it up for transportation.
-			var source = buffer
-					.join( "&" )
-					.replace( /%20/g, "+" )
-				;
-
-			return( source );
-
-		}
-
-	}
-);
-	
 /**
  * Login controller.
  * */
-app.controller('LoginController', function($http) {
+.controller('LoginController', ['$scope', '$http', function($scope, $http, transformRequestAsFormPost) {
 
 	/**
 	 * Login function.
 	 */
 	this.login = function login() {
-		    this.authMsg = '';
-		    this.m_username = 'admin';
-		    this.m_password = 'admin';
+			$scope.login.authMsg = '';
+			$scope.login.m_username = 'admin';
+			$scope.login.m_password = 'admin';
 
 		var request = $http({
 			method: "POST",
 			url: "http://soa-futclubmanager.herokuapp.com/api/oauth/token",
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'},
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 			transformRequest: transformRequestAsFormPost,
 			data: {
 				grant_type:'password',
@@ -107,12 +42,53 @@ app.controller('LoginController', function($http) {
 				password:'admin'
 			}
 		}).success(function (err, data) {
+			$scope.login.m_username = 'ehh';
 
 			console.log("deal with it bitch");
 
 		}).error(function (err, data) {
+			$scope.login.m_username = ':  (';
 			console.log("error you bitch");
 		});
 	};
-});
+}])
 
+/**
+ * transform request factory.
+ */
+.factory('transformRequestAsFormPost', function() {
+		// I prepare the request data for the form post.
+		function transformRequest(data, getHeaders) {
+			var headers = getHeaders();
+			// headers[ "Content-Type" ] = "application/x-www-form-urlencoded";
+			return(serializeData(data));
+		}
+
+		// Return the factory value.
+		return(transformRequest);
+
+		function serializeData(data) {
+			// If this is not an object, defer to native stringification.
+			if (!angular.isObject(data)) {
+				return((data == null) ? "" : data.toString());
+			}
+			var buffer = [];
+			// Serialize each key in the object.
+			for (var name in data) {
+				if (!data.hasOwnProperty(name)) {
+					continue;
+				}
+				var value = data[name];
+				buffer.push(encodeURIComponent(name) +
+					"=" +
+					encodeURIComponent((value == null) ? "" : value)
+				);
+			}
+
+			// Serialize the buffer and clean it up for transportation.
+			var source = buffer
+					.join("&")
+					.replace(/%20/g, "+");
+			return(source);
+		}
+})
